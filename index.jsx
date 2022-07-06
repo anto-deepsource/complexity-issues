0d79b08c5eabc513a26aa6abed1f3c3100d68e9a
Anto Christopher <anto@deepsource.io>
2022-07-06 17:06:23 +0530

    Check jsx-max-depth

--- index.jsx
+++ index.jsx
@@ -1,7 +1,17 @@
 <App>
   <Foo>
     <Bar>
-      <Baz />
+      <Baz>
+        <Baz1>
+          <Baz2>
+            <Baz3>
+              <Baz4>
+                
+              </Baz4>
+            </Baz3>
+          </Baz2>
+        </Baz1>
+      </Baz>
     </Bar>
   </Foo>
 </App>
