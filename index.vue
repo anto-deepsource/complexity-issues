18d9cbe04c05b3148858cfdd6587cf55f657d9d1
Anto Christopher <anto@deepsource.io>
2022-08-08 11:07:59 +0530

    More nuxt check

--- index.vue
+++ index.vue
@@ -2,5 +2,15 @@
   <div>
     <NuxtLink to="/product">Product</NuxtLink>
     <NuxtLink to="/product">Product</NuxtLink>
+    <Nuxt-Link to="/product">Product</Nuxt-Link>
+    <n-Link to="/product">Product</n-Link>
+
+    <NuxtLink href="/product">Product</NuxtLink>
+    <NuxtLink href="/product" to="/product">Product</NuxtLink>
+    <nuxt-link href="/product" to="/product">Product</nuxt-link>
+    <N-link href="/product" :to="`/product/${id}`">Product</N-link>
+    <NuxtLink href="/product" to="/product" someOtherProp="someOtherValue">Product</NuxtLink>
+    <NLink href="/product">Product</NLink>
+    <n-link href="/product">Product</n-link>
   </div>
 </template>
