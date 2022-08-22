5382ae9bd2662f37e93d4e23cb8f9eed40b38a04
Anto Christopher <anto@deepsource.io>
2022-08-22 13:00:21 +0530

    Cycle 110 test

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get((req, res) => vm.runInThisContext(req.params.code)) // unsafe vm
 
 const a = jwt.sign({ foo: 'bar' }, key, { algorithm: 'none' }) // unsafe jwt
 
-const arr = [{name: 'eslint'}];
+const arr = [{ name: 'eslint' }];
 arr.map(item => item.name); // unused return value
 
 const connection = tls.connect(443, 'www.abc.com', {
