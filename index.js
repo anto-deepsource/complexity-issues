90c7a2c11c329b74600054e7684a4a5a7cd72d92
Anto Christopher <anto@deepsource.io>
2022-10-03 13:00:12 +0530

    Autofix test

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-const init = async (core, proc) => {	
-
+const init = async (core, proc) => {
+  console.log(1)
 }
 
-const cli = async (argv = [], opts = {}) => {	
-
+const cli = async (argv = [], opts = {}) => {
+  console.log(1)
 }
 
-const myAdapter = (core) => ({	
+const myAdapter = (core) => ({
   readdir: async (path, options) => [],
 });
 
 
-test("should create server auth adapter", async () => {		
+test("should create server auth adapter", async () => {
   return expect(basicScaffold("auth", "server")).resolves.toBe(true);
 });
 
 (
-  async function init() {	
-
+  async function init() {
+    console.log(1)
   }
 )();
 
 module.exports = {
-  async listen() {	
+  async listen() {
+    console.log(1)
   },
 
   init: async () => {
-
+    console.log(1)
   },
 
   readdir: (vfs) => async (path) => [],
