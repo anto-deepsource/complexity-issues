f6b499026c55cc5c3db117311347368b3e62dbd2
Anto Christopher <anto@deepsource.io>
2022-11-22 13:01:36 +0530

    ancestry fp test

--- index.js
+++ index.js
@@ -18,3 +18,15 @@ const a = jwt.sign({ foo: 'bar' }, key, { algorithm: 'none' }) // unsafe jwt
 
 const arr = [{name: 'eslint'}];
 arr.map(item => item.name); // unused return value
+
+const aa = 1
+
+foo()
+
+function foo() {
+  if (aa === 1) {
+    return 1
+  } else {
+    return 2
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-const aa: string = 'a'
+const aa = 1
+
+foo()
+
+function foo() {
+  if (aa === 1) {
+    return 1
+  } else {
+    return 2
+  }
+}
