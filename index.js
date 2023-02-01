76b36441146b0fb7714a6ad3d5fba687925e0b85
Anto Christopher <anto@deepsource.io>
2023-02-01 22:00:33 +0530

    LM Suggestions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const comp = <span sx={{ verticalAlign: 'super' }}>Unleashing Possibilities</span>
+const comp = <span sx={{ verticalAlign: 'super' }}>Unleashing Possibilitiess</span>
 
 class Foo {
   bar() {
