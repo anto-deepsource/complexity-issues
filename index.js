2983c0fba5d7c185741794ac8c417731734b72d3
Anto Christopher <anto@deepsource.io>
2023-02-06 13:31:15 +0530

    d1001 test

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const vm = require('vm')
+const jwt = require('jsonwebtoken')
+
+for (const x in xs) { if (x) { /* code */ } } // no error
+
+for (const x in xs) { console.log('x') } // error
+
+for (const y in ys) { // error
+  for (const x in xs) { // error
+    console.log(x, y)
+  }
+}
+
+const pattern = /[0-9][:alpha:]/ // posix in regex
+app.get((req, res) => vm.runInThisContext(req.params.code)) // unsafe vm
+
+const a = jwt.sign({ foo: 'bar' }, key, { algorithm: 'none' }) // unsafe jwt
+
+const arr = [{name: 'eslint'}];
+arr.map(item => item.name); // unused return value
+
 function App() {
   return 0;
 }
