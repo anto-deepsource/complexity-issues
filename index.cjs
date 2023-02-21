103f589b0f72c0503e626c55c730042aa6ba2de7
Anto Christopher <anto@deepsource.io>
2023-02-21 09:56:30 +0530

    standard test

---
+++ index.cjs
@@ -0,0 +1,20 @@
+for(i=0;i<1;i++){console.log(i); debugger;}
+if (i) {
+
+} 
+else {
+
+}
+function render () {
+  return (
+    <div><h1>User Component</h1>
+      Hello, <b>{this.props.name}</b>
+      <br />
+      You are <b>{this.props.age} years old</b>
+      <br />
+      You live at: <b>{this.props.address}</b>
+      <br />
+      You were born: <b>{this.props.dob.toDateString()}</b>
+    </div>
+  )
+}
