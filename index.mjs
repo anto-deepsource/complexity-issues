f405f988135ce63ed90e9cf5a37b6ca428e37b9c
Anto Christopher <anto@deepsource.io>
2023-02-21 12:41:44 +0530

    Prettier final test

--- index.mjs
+++ index.mjs
@@ -6,15 +6,13 @@ else {
 
 }
 function render () {
-  return (
-    <div><h1>User Component</h1>
+  return (<div><h1>User Component</h1>
       Hello, <b>{this.props.name}</b>
       <br />
       You are <b>{this.props.age} years old</b>
       <br />
       You live at: <b>{this.props.address}</b>
       <br />
-      You were born: <b>{this.props.dob.toDateString()}</b>
-    </div>
+      You were born: <b>{this.props.dob.toDateString()}</b></div>
   )
 }
