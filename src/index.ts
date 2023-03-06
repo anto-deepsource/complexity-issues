dbd1e642a3499cfad8ba360b0c3fd6692ff02b5b
Anto Christopher <anto@deepsource.io>
2023-03-06 14:58:46 +0530

    test: release v2.7.0

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-const aa: string = 'a'
+import { BrowserWindow } from 'electron'
+
+const mainWindow = new BrowserWindow({
+  webPreferences: {
+    contextIsolation: false // `contextIsolation` should not be set to false
+  }
+})
