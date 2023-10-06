c28c297e7a2ba2847c976431c242883f07919b5d
Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>
2023-10-06 12:08:55 +0530

    Create default.vue

---
+++ layout/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    bodyAttrs: {
+      class: 'h-screen bg-white font-sans antialiased',
+    },
+  },
+}
+</script>
